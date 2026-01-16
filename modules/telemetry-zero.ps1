b254d7bbb6f7e1d06e2094106daa3644b9ba527f
umemiya <elanie477@gmail.com>
2026-01-16 03:31:10 +0000

    Refactor telemetry-zero.ps1 to encapsulate telemetry disabling logic in a function and improve clarity of operations

--- modules/telemetry-zero.ps1
+++ modules/telemetry-zero.ps1
@@ -1,14 +1,16 @@
-# Disable telemetry
-reg add "HKLM\Software\Policies\Microsoft\Windows\DataCollection" `
- /v AllowTelemetry /t REG_DWORD /d 0 /f
+function Apply-TelemetryPolicy {
 
-reg add "HKLM\Software\Microsoft\Windows\DataCollection" `
- /v DoNotShowFeedbackNotifications /t REG_DWORD /d 1 /f
+    Write-Host "[-] Disabling telemetry"
 
-# Disable Advertising ID
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo" `
- /v Enabled /t REG_DWORD /d 0 /f
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Force | Out-Null
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" `
+        -Name "AllowTelemetry" -Type DWord -Value 0
 
-# Disable app tracking
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
- /v Start_TrackProgs /t REG_DWORD /d 0 /f
+    foreach ($svc in @("DiagTrack","dmwappushservice")) {
+        $s = Get-Service $svc -ErrorAction SilentlyContinue
+        if ($s) {
+            Stop-Service $svc -Force -ErrorAction SilentlyContinue
+            Set-Service $svc -StartupType Disabled
+        }
+    }
+}
